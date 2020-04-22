f309347264dd278f5288714411987107fd775702
Shah <shah.ali.web@Hotmail.com>
2020-04-22 17:22:57 -0400

    added hw8

---
+++ main.js
@@ -0,0 +1,32 @@
+function getData () {
+	const movie = document.getElementById("movie").value;
+	const container = document.getElementById("container");
+	const url = `https://www.omdbapi.com/?apikey=5166b123&t=${movie}`;
+	const xhttps = new XMLHttpRequest();
+	
+	xhttps.onreadystatechange = function () {
+		if (this.readyState === 4 && this.status === 200) {
+			console.log(this.responseText);
+			console.log(JSON.parse(this.responseText));
+			const data = JSON.parse(this.responseText);
+			
+			
+			document.getElementById("movie-title").innerHTML = data.Title;
+			document.getElementById("poster").src = data.Poster;
+			document.getElementById("year").innerHTML = data.Year;
+			document.getElementById("released").innerHTML = data.Released;
+			document.getElementById("runtime").innerHTML = data.Runtime;
+			document.getElementById("director").innerHTML = data.Director;
+			document.getElementById("writer").innerHTML = data.Writer;
+			document.getElementById("genre").innerHTML = data.Genre;
+			document.getElementById("plot").innerHTML = data.Plot;
+		}
+	};
+	xhttps.open("GET", url, true);
+	xhttps.send();
+}
+const button = document.getElementById("btn");
+button.addEventListener("click", () => {
+	getData();
+	document.getElementById("container");
+});
